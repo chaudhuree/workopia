f2a5ea78965ea7ebce3a95628d573914086db741
chaudhuree <chaudhuree00@gmail.com>
2023-12-25 19:56:07 +0600

    basic router created . fix docment root in xampp https://www.notion.so/how-to-change-document-root-in-xampp-f1de84729010426ca27c46ee6bb95603

---
+++ controllers/home.php
@@ -0,0 +1,1 @@
+<?php loadView('home') ?>
--- public/index.php
+++ public/index.php
@@ -1,4 +1,24 @@
-<?php 
+<?php
 require '../helper.php';
 // require basePath('views/home.view.php');
-loadView('home');
+// loadView('home');
+
+
+
+
+
+
+$routes = [
+  '/' => 'controllers/home.php',
+  '/listings' => 'controllers/listings/index.php',
+  '/listings/create' => 'controllers/listings/create.php',
+  '404' => 'controllers/error/404.php',
+];
+
+$uri = $_SERVER['REQUEST_URI'];
+// echo "path ".$uri;
+if (array_key_exists($uri, $routes)) {
+  require basePath($routes[$uri]);
+} else {
+  require basePath($routes['404']);
+}

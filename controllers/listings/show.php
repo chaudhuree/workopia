b989cc0da950cf2b3cb256098ebb346b89004158
chaudhuree <chaudhuree00@gmail.com>
2024-01-01 05:10:23 +0600

    Display single fetched data to the view

--- controllers/listings/show.php
+++ controllers/listings/show.php
@@ -12,6 +12,8 @@ $params = [
 
 $listing = $db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
 
-inspect($listing);
+// inspect($listing);
 
-loadView('listings/show');
+loadView('listings/show', [
+  'listing' => $listing
+]);

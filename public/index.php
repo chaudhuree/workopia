80fcd536c2784105fa3d43c813c4a5aa41389007
chaudhuree <chaudhuree00@gmail.com>
2024-01-03 01:29:37 +0600

    custom autoloader

--- public/index.php
+++ public/index.php
@@ -1,7 +1,14 @@
 <?php
 require '../helpers.php';
-require basePath('Framework/Router.php');
-require basePath('Framework/Database.php');
+// require basePath('Framework/Router.php');
+// require basePath('Framework/Database.php');
+
+// custom autoloader
+spl_autoload_register(function ($class) {
+  // require basePath("Framework/{$class}.php");
+  require basePath('Framework/' . $class . '.php');
+});
+
 
 // Instatiate the router
 $router = new Router();
